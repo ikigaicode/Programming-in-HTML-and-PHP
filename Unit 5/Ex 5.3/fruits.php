b528821dbdf1effa09bc96e67e37e87b023f65d6
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2019-10-28 19:26:59 -0700

    2nd example

---
+++ Unit 5/Ex 5.3/fruits.php
@@ -0,0 +1,17 @@
+<?php
+  $fruit = "avocado";
+
+  switch ($fruit) {
+    case "apple":
+      echo "This is an apple.";
+      break;
+    case "orange":
+      echo "This is an orange";
+      break;
+    case "banana";
+      echo "This is a banana";
+      break;
+    default:
+      echo "This is not an allowed fruit treat.";
+  }
+ ?>
