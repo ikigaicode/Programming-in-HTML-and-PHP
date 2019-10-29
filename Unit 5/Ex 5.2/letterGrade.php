3c6cf3e037f62a9d00500e7f05fe7ce0fc39c316
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2019-10-28 18:43:23 -0700

    5.4 Conditional Execution

---
+++ Unit 5/Ex 5.2/letterGrade.php
@@ -0,0 +1,10 @@
+<?php
+  $grade = 87;
+
+  if ($grade >= 90) echo "A <br />";
+    elseif ($grade >= 80) echo "B <br />";
+    elseif ($grade >= 70) echo "C <br />";
+    elseif ($grade >= 60) echo "D <br />";
+    else echo "F <br />";
+
+?>
