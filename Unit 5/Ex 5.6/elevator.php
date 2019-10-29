559919b5e10b5654b372b9a594aaec487a4a0dd3
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2019-10-28 20:10:29 -0700

    5.5.2 Conditional Loops

---
+++ Unit 5/Ex 5.6/elevator.php
@@ -0,0 +1,25 @@
+<?php
+  echo "The elevator problem... <br />";
+
+  $limit = 500;
+
+  echo "maximun weight = " .$limit. " pounds <br />";
+    $totalWeight = 0; $maxWeight = 500;
+
+  do {
+    $newWeight = rand (0, $maxWeight);
+    if (($totalWeight + $newWeight) <= $limit) {
+      $totalWeight += $newWeight;
+      echo "New weight = " .$newWeight. ",
+            Total weight = " .$totalWeight. "<br />";
+      $newWeight = 0;
+    }
+      else echo "You weigh " .$newWeight. "pounds.
+            I'm sorry, but you can't get on. <br />"
+  } while (($totalWeight + $newWeight) <= $limit);
+ ?>
+
+
+
+//$totalWeight += $newWeight;
+//$totalWeight = $totalWeight + $newWeight;
