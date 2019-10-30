2ca27003e91b3c6d49d54d6421bd62dc1836f8bc
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2019-10-29 19:17:34 -0700

    5.6.2 Newton’s Square Root Algorithm

---
+++ Unit 5/Ex 5.8/newton.php
@@ -0,0 +1,29 @@
+<html>
+  <head>
+    <title></title>
+  </head>
+
+  <body>
+    <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+      <h3>Newton's Square Root Algorithm</h3>
+
+      Enter a positive number:
+
+      <input type="text" value="9" size="4" name="n" />;
+      <br />
+      <input type="submit" value="Click to calculate square root" />;
+
+    </form>
+  </body>
+</html>
+
+<?php
+  $n = $_POST["n"];
+  $g = $n / 2;
+
+  do {
+    $g = ($g + $n / $g) / 2.;
+  } while (abs ($g * $g - $n) > 1e - 5);
+
+  echo "Square Root = " .$g. "<br />";
+?>
