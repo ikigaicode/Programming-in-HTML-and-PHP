f1400e78ac05cf6e56509d9ac7eeb1b324d7403c
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2019-10-29 18:42:20 -0700

    empty function.

--- Unit 5/Ex 5.7/quadrat1.php
+++ Unit 5/Ex 5.7/quadrat1.php
@@ -22,24 +22,27 @@
 </html>
 
 <?php
-  $a = $_POST["a"];
-  $b = $_POST["b"];
-  $c = $_POST["c"];
+  if (! empty ($_POST)) {
+    $a = $_POST["a"];
+    $b = $_POST["b"];
+    $c = $_POST["c"];
 
-  $d = $b * $b - 4 * $a * $c;
+    $d = $b * $b - 4 * $a * $c;
 
-  if ($d == 0) {
-    $r1 = $b / (2 * $a);
-    $r2 = "undefined";
-  }
-  elseif ($d < 0) {
-    $r1 = "undefined";
-    $r2 = "undefined";
-  }
-  else {
-    $r1 = (-$b + sqrt($b * $b - 4 * $a * $c)) / 2 / $a;
-    $r2 = (-$b - sqrt($b * $b - 4 * $a * $c)) / 2 / $a;
+    if ($d == 0) {
+      $r1 = $b / (2 * $a);
+      $r2 = "undefined";
+    }
+    elseif ($d < 0) {
+      $r1 = "undefined";
+      $r2 = "undefined";
+    }
+    else {
+      $r1 = (-$b + sqrt($b * $b - 4 * $a * $c)) / 2 / $a;
+      $r2 = (-$b - sqrt($b * $b - 4 * $a * $c)) / 2 / $a;
+    }
+
+    echo "r1 = " .$r1. ", r2 = " .$r2;
   }
 
-  echo "r1 = " .$r1. ", r2 = " .$r2.;
 ?>
