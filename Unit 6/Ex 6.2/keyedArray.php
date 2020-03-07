0e2c72520c6e7cb342edfe8b8da8857d6dc3d04b
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2020-03-06 18:04:30 -0700

    back tick to single quote errors.

--- Unit 6/Ex 6.2/keyedArray.php
+++ Unit 6/Ex 6.2/keyedArray.php
@@ -1,10 +1,10 @@
 <?php
   // Create an array with user-specified keys...
-  echo ´<br />A keyed array:<br />´;
+  echo '<br />A keyed array:<br />';
 
-  $stuff = array(´mine´ => ´BMW´, ´yours´ => ´Lexus´, ´ours´ => ´house´);
+  $stuff = array('mine' => 'BMW', 'yours' => 'Lexus', 'ours' => 'house');
   foreach ($stuff as $key => $val) {
-    echo ´$stuff[´ . $key . ´]=´ .$val . ´<br />´;// code...
+    echo '$stuff[' . $key . ']=' .$val . '<br />';// code...
   }
 
  ?>
