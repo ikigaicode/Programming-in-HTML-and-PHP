b46f0e3c97b8bfac5d10bc5d76a1ac4216f12a64
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2020-03-08 21:56:16 -0700

    1st try

---
+++ Unit 6/Ex 6.8/sort3.php
@@ -0,0 +1,23 @@
+<?php
+function compare ($x, $y) {
+  return strcasecmp ($x, $y);
+}
+
+// Create and sort an array...
+
+$a = array('Xena', 'Sue', 'david', 'apple');
+
+echo "Original array: <br />";
+
+for ($i=0; $i<sizeof($a); $i++) {
+  echo $a[$i] . "<br />";
+}
+
+echo "Sorted array with user-defined comparisions of elements: <br />";
+
+usort($a, "compare");
+
+for ($i=0; $i<sizeof($a); $i++) {
+  echo $a[$i] . "<br />";
+}
+ ?>
