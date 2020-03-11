5f2c9b874d33c53db2b3805108b5e5f56d580286
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2020-03-10 18:28:40 -0700

    1st try

---
+++ Unit 6/Ex 6.10b/quadrat2.php
@@ -0,0 +1,27 @@
+<?php
+
+var_dump ($_POST["coeff"]);
+echo "<br />";
+
+$coefficientArray = array_keys ($_POST["coeff"]);
+
+$a = $_POST["coeff"][$coefficientArray[0]];
+$b = $_POST["coeff"][$coefficientArray[1]];
+$c = $_POST["coeff"][$coefficientArray[2]];
+$d = $b * $b - 4.0 * $a * $c;
+
+if ($d == 0) {
+  $r1 = $b/(2.0 * $a);
+  $r2 = "undefined";
+}
+else if ($d < 0) {
+  $r1 = "undefined";
+  $r2 = "undefined";
+}
+else {
+  $r1 = (-$b + sqrt($b * $b - 4.0 * $a * $c)) / (2.0 * $a);
+  $r2 = (-$b + sqrt($b * $b - 4.0 * $a * $c)) / (2.0 * $a);
+}
+
+echo "r1 = " . $r1 . ", r2 = " . $r2;
+?>
