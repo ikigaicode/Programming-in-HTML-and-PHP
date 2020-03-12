29104e8720b30d97555fd6ae55abde8ac370c6d7
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2020-03-12 05:30:09 -0700

    1st try

---
+++ Unit 7/Ex 7.8/eplerEquation.php
@@ -0,0 +1,68 @@
+<html>
+  <head>
+    <title>Calculate true anomaly of orbiting object</title>
+  </head>
+
+  <body>
+    <h3>Calculate true anomaly of orbiting object</h3>
+
+    <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+
+      Semimajor axis (Km) :
+      <input type="text" name="a" size="5" value="7000" /><br />
+
+      Eccentricity (0 to &lt;1) :
+      <input type="text" name="e" size="5" value="0.7" /><br />
+
+      Time steps along orbit path (how many, even #) :
+      <input type="text" name="n" size="3" value="10" /><br />
+
+      <input type="submit" value="Click here to generate true anomalies." />
+
+    </form>
+  </body>
+</html>
+
+<?php
+   $a = $_POST["a"];
+   $e = $_POST["e"];
+   $n = $_POST["n"];
+   $tau = 2.0 * pi() * $a * sqrt($a/398601.2);
+   $dt = ($tau/60) / $n;
+
+   echo "Period, minutes: " . round($tau/60, 3) . "<br />"
+   echo "time, mean anomaly true anomaly (deg) <br />";
+
+    for ($t=0; $t<($tau/60); $t+=$dt){
+      $M=2*pi()*$t*60/$tau;
+      echo round($t,4) . " " . round(180/pi()*$M,3);
+      if ($e == 0) {
+          $f=$M; // for circular orbit
+      }
+        else {
+        $E=getE($e,$M,$M); // E = M as initial value
+        $f=acos((cos($E)-$e)/(1-$e*cos($E)));
+        }
+
+        $f=180/pi()*$f;
+
+        if ($t/($tau/60) > 0.5) {
+            $f=360-$f;
+        }
+        echo " ".round($f,3)."<br />";
+    }
+
+    echo round($tau/60,3)." ".round(360,3) ." ". round(360,3)."<br />";
+
+    function getE($e,$M,$E) {
+      $newE=$M + $e*sin($E);
+      if (abs($newE-$E) < 1e-5) {
+        return $newE;
+      }
+      else {
+        return getE($e,$M,$newE); // recursive
+      }
+
+
+
+?>
