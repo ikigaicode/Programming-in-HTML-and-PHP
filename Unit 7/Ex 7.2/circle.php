b97234bbcf0c31e839e31b2064ed93ddcd8c4f8b
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2020-03-11 21:40:24 -0700

    ( ! ) Notice: Undefined index: r in C:\wamp64\www\Ex 7.2\circle.php on line 18

--- Unit 7/Ex 7.2/circle.php
+++ Unit 7/Ex 7.2/circle.php
@@ -15,16 +15,18 @@
 </html>
 
 <?php
-$r =  $_POST["r"];
-$a = []; // [0] is circumference, [1] is area
-$a = circleStuff($r);
-echo "circumference: " . $a[0] . "area: " . $a[1] . "<br />";
 function circleStuff($r) {
   $a = [];
   $a [0] = round(2.0 * pi () * $r, 2); // circumference in [0]
   $a [1] = round(pi() * $r * $r, 2); // area in [1]
   return $a;
 }
+
+$r =  $_POST["r"];
+$a = []; // [0] is circumference, [1] is area
+$a = circleStuff($r);
+echo "circumference: " . $a[0] . "area: " . $a[1] . "<br />";
+
 
 
  ?>
