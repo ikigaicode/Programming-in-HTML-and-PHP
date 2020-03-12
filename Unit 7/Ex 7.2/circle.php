a1667fc500c4e6197cc3ed12396f9c8387986fab
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2020-03-11 21:43:09 -0700

    ( ! ) Parse error: syntax error, unexpected end of file in C:\wamp64\www\Ex 7.2\circle.php on line 34

--- Unit 7/Ex 7.2/circle.php
+++ Unit 7/Ex 7.2/circle.php
@@ -23,11 +23,9 @@ function circleStuff($r) {
 }
 
 if (!empty($_POST)){
-$r =  $_POST["r"];
-$a = []; // [0] is circumference, [1] is area
-$a = circleStuff($r);
-echo "circumference: " . $a[0] . "area: " . $a[1] . "<br />";
-
-
-
+  $r =  $_POST["r"];
+  $a = []; // [0] is circumference, [1] is area
+  $a = circleStuff($r);
+  echo "circumference: " . $a[0] . "area: " . $a[1] . "<br />";
+}
  ?>
