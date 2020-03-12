2810182ab0781865d22e9c2cee29b49438e1b290
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2020-03-12 03:50:22 -0700

    Adding curly brackets in the function declaration and changing lines of code.

--- Unit 7/Ex 7.7/legendre.php
+++ Unit 7/Ex 7.7/legendre.php
@@ -8,9 +8,15 @@
      echo $i . " " . $L . "<br />";
    }
    function getLegendre($n, $x){
-     if ($n == 0) return 1;
-     else if ($n == 1) return $x;
-     else return (2.0 * $n-1) / $n * getLegendre  ($n-1, $x) -
-      ($n-1) / $n * getLegendre($n-2, $x);
+     if ($n == 0){
+       return 1;
+     }
+     else if ($n == 1) {
+       return $x;
+     }
+     else {
+       return (2.0 * $n-1) / $n * getLegendre  ($n-1, $x) -
+       ($n-1) / $n * getLegendre($n-2, $x);
+     }
    }
 ?>
