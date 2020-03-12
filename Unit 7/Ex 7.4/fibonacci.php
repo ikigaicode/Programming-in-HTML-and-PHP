b60d160f2da0efe8e489b3f55dad322d67d5d259
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2020-03-12 01:47:32 -0700

    1st try

---
+++ Unit 7/Ex 7.4/fibonacci.php
@@ -0,0 +1,13 @@
+<?php
+  $n = 8;
+  echo "Calculate the first" . $n . "Fibonacci numbers. <br />";
+
+  for ($i=1; $i<=$n; $i++) {
+    echo $i . " " . Fib($i) . "<br />";
+  }
+
+  function Fib($n) {
+    if ($n<=2) return 1;
+    else return Fib($n-1)+Fib($n-2);
+  }
+?>
