b1dff0e2c53d49383545d265af10e99b0b8ae0fc
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2020-04-08 18:38:43 -0700

    Blanck Space in line 17, between %4d and %'02d

--- Unit 8/Ex 8.1/formatTest.php
+++ Unit 8/Ex 8.1/formatTest.php
@@ -14,7 +14,7 @@
   fprintf ($out, "display number as a string\r\n");
   fprintf ($out, "%s \r\n", $b);
   $line1 = "1/14/2013 17:3:1";
-  $formatString = "%'02d/ %'02d/ %4d%'02d:%'02d:%'02d";
+  $formatString = "%'02d/ %'02d/ %4d %'02d:%'02d:%'02d";
   sscanf ($line1, "%i/%i/%i %d:%d:%d", $mon, $day, $yr, $hr, $min, $sec);
   fprintf ($out, "Display %s padded with 0's: ", $line1);
   fprintf ($out, $formatString, $mon, $day, $yr, $hr, $min, $sec);
