51c7a75d8e5da590187e6fda7a6551a142a7db06
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2020-04-08 19:14:15 -0700

    is a \n not /n, line 14.

--- Unit 8/Ex 8.2/siteFile.php
+++ Unit 8/Ex 8.2/siteFile.php
@@ -3,7 +3,7 @@
   $out = fopen("siteFile.csv",'w');
   $header = fgets($in);
   $h = array();
-  $h = explode('', $header);
+  $h = explode(' ', $header);
   echo $header. "<br />";
   fprintf ($out, "%s, %s, %s", $h[ 0], $h[ 1], $h[ 2]);
   while (!feof($in)){
@@ -11,7 +11,7 @@
     if (strlen($line)>3){
       sscanf ($line, "%s %f %f", $site, $lon, $lat);
       echo $site. ",".$lon.",".$lat."<br />";
-      fprintf ($out, "%s, %f, %f/n", $site, $lon, $lat);
+      fprintf ($out, "%s, %f, %f\n", $site, $lon, $lat);
     }
   }
   fclose ($in);
