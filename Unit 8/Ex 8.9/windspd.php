dba8540e2a9f7795e8428c029ccee53732cffc70
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2020-04-10 17:57:57 -0700

    1st try

---
+++ Unit 8/Ex 8.9/windspd.php
@@ -0,0 +1,27 @@
+<?php
+  $inFile = "windspd.data";
+  $outFile = "windspd.out";
+  $in = fopen($inFile, "r") or die("Can't open file.");
+  $out = fopen($outFile, "w");
+  while (!feof($in)){
+    //Read one month, year, # of days.
+    fscanf($in, "%u %u %u", $m, $y, $nDays);
+    if (feof($in)) exit;
+    echo $m. ', ' .$y. ', ' .$nDays. '<br />';
+    $nMissing = 0;
+    for ($i = 1; $i<=$nDays; $i++){
+      $hrly_string = fgets($in);
+      $hrly = explode(', ', $hrly_string);
+       for ($hr=0; $hr<24; $hr++){
+        if ($hrly[ $hr] == -1) $nMissing++;
+       }
+     }
+
+  echo 'Number of missing hours this month is'.
+    $nMissing. '.<br />';
+  fprintf($out, "%u, %u, %u\r\n", $m, $y, $nMissing);
+}
+echo "All done.<br />";
+//fclose($in);
+//fclose($in);
+?>
