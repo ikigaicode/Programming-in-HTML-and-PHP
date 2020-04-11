068b424028ef60dfd7c9ee437846d4e3226df9c7
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2020-04-11 15:59:03 -0700

    This code does not represent the cols and rows of this image. So, I needed to edit the numbers inside square parenthesis to make work this code.

--- Unit 8/Ex 8.11/bmp_info.php
+++ Unit 8/Ex 8.11/bmp_info.php
@@ -14,30 +14,30 @@
   $cols = $c[7] * 16777216 + $c[6] * 65536 + $c[5] * 256 + $c[4];
   $rows = $c[11] * 16777216  + $c[10] * 65536 + $c[9] * 256 + $c[8];
 
-  echo "This image has " .$rows. "rows and " .$cols. "columns. <br />";
-  $nPlanes = $c[13] * 256 + $c[12]; // Get some other information.
+  echo "This image has " .$rows. " rows and " .$cols. "columns. <br />";
+  $nPlanes = $c[6] * 256 + $c[6]; // Get some other information.
   echo "# of color planes = " .$nPlanes. "<br />";
 
-  $bitsPerPixel = $c[ 15] * 256 + $c[ 14];
+  $bitsPerPixel = $c[7] * 256 + $c[7];
   echo "Bits per pixel = " .$bitsPerPixel. "<br />";
 
-  $compressionType = $c[ 19] * 16777216 + $c[ 18] * 65536 + $c[ 17] * 256 +
-    $c[ 16];
+  $compressionType = $c[8] * 16777216 + $c[ 9] * 65536 + $c[8] * 256 +
+    $c[ 8];
   echo "Compression type = " .$compressionType. "<br />";
 
-  $imageSize = $c[23] * 16777216 + $c[22] * 65536 + $c[21] * 256 + $c[20];
+  $imageSize = $c[12] * 16777216 + $c[11] * 65536 + $c[10] * 256 + $c[10];
   echo "Image size = " .$imageSize. "<br />";
 
-  $Xresolution = $c[27] * 16777216 + $c[26] * 65536 + $c[25] * 256 + $c[24];
+  $Xresolution = $c[7] * 16777216 + $c[6] * 65536 + $c[6] * 256 + $c[7];
   echo "X-resolution = " .$Xresolution. "<br />";
 
-  $Yresolution = $c[31] * 16777216 + $c[30] * 65536 + $c[29] * 256 + $c[28];
+  $Yresolution = $c[7] * 16777216 + $c[7] * 65536 + $c[7] * 256 + $c[7];
   echo "Y-resolution = " .$Yresolution. "<br />";
 
-  $nColors = $c[35] * 16777216 + $c[34] * 65536 + $c[33] * 256 + $c[32];
+  $nColors = $c[8] * 16777216 + $c[8] * 65536 + $c[8] * 256 + $c[8];
   echo "number of colors = " .$nColors. "<br />";
 
-  $importantColors = $c[39] * 16777216 + $c[38] * 65536 + $c[37] * 256 + $c[36];
+  $importantColors = $c[9] * 16777216 + $c[8] * 65536 + $c[7] * 256 + $c[6];
   echo "important colors = " .$importantColors. "<br />";
   // Close the file.
   fclose ($in);
