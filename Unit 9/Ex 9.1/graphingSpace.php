84aa3632e7c207b741aa8bca1319351c932c18b7
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2020-04-11 23:00:31 -0700

    some blank space added.

--- Unit 9/Ex 9.1/graphingSpace.php
+++ Unit 9/Ex 9.1/graphingSpace.php
@@ -1,5 +1,6 @@
 <?php
   Header ("Content-type: image/gif");
+
   //define title.
   $TitleString = "Chart Title";
   // dimensions of plotting space.
@@ -14,20 +15,24 @@
   // axis lengths.
   $x_length = 180;
   $y_length = 120;
+
   // create image space.
   $im = ImageCreate($x_max, $y_max) or die ("Cannot Initialize new GD image stream");
   // define colors -- first call fills background.
   $background_color = ImageColorAllocate ($im, 200, 200, 200);
   // define text color.
   $black = ImageColorAllocate($im, 0, 0, 0);
+
   // display text
   ImageString($im, 5, 0, 0, "(0, 0)", $black);
   ImageString($im, 5, $x_title, $y_title, $TitleString, $black);
   ImageString($im, 5, $x_max-90, $y_max-16, "(300, 200)", $black);
+
   // draw x-y axis.
   ImageSetThickness($im, 2);
   ImageLine($im, $x0, $y0, $x0 + $x_length, $y0, $black);
   ImageLine($im, $x0, $y0, $x0, $y0 - $y_length, $black);
+
   // display image.
   ImageGIF($im);
   // release resources.
