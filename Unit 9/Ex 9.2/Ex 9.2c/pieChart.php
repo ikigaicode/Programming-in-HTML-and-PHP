632918778d7ee542994a3274549756b328642077
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2020-04-14 05:59:13 -0700

    First attempt

--- Unit 9/Ex 9.2/Ex 9.2c/pieChart.php
+++ Unit 9/Ex 9.2/Ex 9.2c/pieChart.php
@@ -1,102 +1,104 @@
 <?php
-  function
-  generatePie($Title, $A, $legends, $x_max, $y_max, $x0, $y0, $legend_size,
+  function generatePie($Title, $A, $legends, $x_max, $y_max, $x0, $y0, $legend_size,
               $dia, $xo_legend, $y0_legend){
-                Header ("Content-type: image/gif");
 
-                //$TitleString = "Pie Chart";
+  Header ("Content-type: image/gif");
 
-                //$A = array(60, 50, 40, 100, 50, 50, 75, 5, 10, 15, 20, 35);
+  //$TitleString = "Pie Chart";
+  //$Title = "Pie Chart";
 
-                //$legends = array("Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7",
-                //                 "Item8", "Item9", "Item10", "Item11", "Item12");
+  //$A = array(60, 50, 40, 100, 50, 50, 75, 5, 10, 15, 20, 35);
 
-                // Dimensions of plotting space.
-                //$x_max = 800;
-                //$y_max = 500;
+  //$legends = array("Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7",
+  //           "Item8", "Item9", "Item10", "Item11", "Item12");
 
-                // Center poiint for pie chart.
-                //$x0 = 200;
-                //$y0 = 250;
+  // Dimensions of plotting space.
+  //$x_max = 800;
+  //$y_max = 500;
 
-                // Diameter of pie.
-                // $dia = 360;
+  // Center poiint for pie chart.
+  //$x0 = 200;
+  //$y0 = 250;
 
-                // Locates plot title.
-                $x_title = 10;
-                $y_title = 20;
+  //Diameter of pie.
+  //$dia = 360;
 
-                // Upper left hand corner of legend space.
-                //$x0_legend = 400;
-                //$y0_legend = 75;
+  // Locates plot title.
+  $x_title = 10;
+  $y_title = 20;
 
-                // Size of legend color boxes.
-                //$legend_size = 25;
+  // Upper left hand corner of legend space.
+  //$x0_legend = 400;
+  //$y0_legend = 75;
 
-                // Vertical spac between legend color boxes.
-                $dy_legend = $legend_size+5;
+  // Size of legend color boxes.
+  $legend_size = 25;
 
-                // Create image space.
-                $im = ImageCreate($x_max, $y_max) or die ("Cannot Initialize new GD image stream");
+  // Vertical spac between legend color boxes.
+  $dy_legend = $legend_size+5;
 
-                // Define some colors.
-                // $background_color creates background.
-                $background_color = ImageColorAllocate ($im, 200, 200, 200);
+  // Create image space.
+  $im = ImageCreate($x_max, $y_max) or die ("Cannot Initialize new GD image stream");
 
-                // First call fills background.
-                $black = ImageColorAllocate($im, 0, 0, 0);
+  // Define some colors.
+  // $background_color creates background.
+  $background_color = ImageColorAllocate ($im, 200, 200, 200);
 
-                // For drawing colors for up 12 sections.
-                // Pie section colors for up to $n_max sections.
-                $ColorCode = array("255, 0, 0", "51, 0, 255", "51, 255, 51", "255, 153, 0",
-                                   "0, 204, 153", "204, 255, 102", "255, 102, 102",
-                                   "102, 204, 255", "204, 153, 255", "255, 51, 153",
-                                   "204, 0, 255", "255, 255, 51");
+  // First call fills background.
+  $black = ImageColorAllocate($im, 0, 0, 0);
 
-                //$n_max = count($ColorCode);
-                $PieColor = array();
-                for ($i=0; $i<12; $i++){
-                  $ColorCodeSplit = explode(',', $ColorCode[ $i]);
-                  $PieColor[ $i] = ImageColorAllocate($im,
-                                    $ColorCodeSplit[0], $ColorCodeSplit[1],
-                                    $ColorCodeSplit[2]);
-                }
+  // For drawing colors for up 12 sections.
+  // Pie section colors for up to $n_max sections.
+  $ColorCode = array("255, 0, 0", "51, 0, 255", "51, 255, 51", "255, 153, 0",
+                "0, 204, 153", "204, 255, 102", "255, 102, 102",
+                "102, 204, 255", "204, 153, 255", "255, 51, 153",
+                "204, 0, 255", "255, 255, 51");
 
-                // Convert data array into angles, total of 360 deg.
-                $sum = array_sum($A);
-                $n = count($A);
-                $start = array();
-                $end = array();
-                $start[ 0] = 0;
-                for ($i=0; $i<$n; $i++){
-                  $slice = $A[ $i] / $sum*360;
-                  if ($i>0) $start[ $i] = $end[ $i-1];
-                  $end [ $i] = $start[ $i] + $slice;
-                }
+  //$n_max = count($ColorCode);
+  $PieColor = array();
+  for ($i=0; $i<12; $i++){
+    $ColorCodeSplit = explode(',', $ColorCode[ $i]);
+    $PieColor[ $i] = ImageColorAllocate($im, $ColorCodeSplit[0], $ColorCodeSplit[1],
+                    $ColorCodeSplit[2]);
+  }
 
-                // Display title.
-                ImageString($im, 5, $x_title, $y_title, $Title, $black);
+  // Convert data array into angles, total of 360 deg.
+  $sum = array_sum($A);
+  $n = count($A);
+  $start = array();
+  $end = array();
+  $start[ 0] = 0;
+  for ($i=0; $i<$n; $i++){
+    $slice = $A[ $i] / $sum*360;
+    if ($i>0) $start[ $i] = $end[ $i-1];
+    $end [ $i] = $start[ $i] + $slice;
+  }
 
-                // Draw filled arcs
-                for ($i=0; $i<$n; $i++){
-                  ImageFilledArc($im, $x0, $y0, $dia, $dia, $start[ $i], $end[ $i], $PieColor[$i],
-                               IMG_ARC_PIE);
-                }
+  // Display title.
+  ImageString($im, 5, $x_title, $y_title, $Title, $black);
 
-                // Display legend.
-                for ($i=0; $i<$n; $i++){
-                  ImageFilledRectangle($im, $x0_legend, $y0_legend + $dy_legend * $i,
-                                     $x0_legend + $legend_size, $y0_legend + $dy_legend * $i +
-                                     $legend_size, $PieColor[$i]);
-                  $legends[$i] = $legends[$i] ." (".number_format($A[$i], 1, ".", ",").")";
-                  ImageString($im, 5, $x0_legend + $legend_size + 5, $y0_legend + $dy_legend * $i + 5,
-                            $legends[$i], $black);
-                }
+  // Draw filled arcs
+  for ($i=0; $i<$n; $i++){
+    ImageFilledArc($im, $x0, $y0, $dia, $dia, $start[ $i], $end[ $i], $PieColor[$i],
+                  IMG_ARC_PIE);
+  }
 
-                // Display and release allocated resources.
-                ImageGIF($im);
-                ImageDestroy($im);
-              }
+  // Display legend.
+  for ($i=0; $i<$n; $i++){
+    ImageFilledRectangle($im, $x0_legend, $y0_legend + $dy_legend * $i,
+                        $x0_legend + $legend_size, $y0_legend + $dy_legend * $i +
+                        $legend_size, $PieColor[$i]);
+
+    $legends[$i] = $legends[$i] ." (".number_format($A[$i], 1, ".", ",").")";
+
+    ImageString($im, 5, $x0_legend + $legend_size + 5, $y0_legend + $dy_legend * $i + 5,
+                $legends[$i], $black);
+  }
+
+  // Display and release allocated resources.
+  ImageGIF($im);
+  ImageDestroy($im);
+}
 
   // MAIN PROGRAM ------------------------------------
   $inFile = $_POST["fileName"];
@@ -127,12 +129,13 @@
     $line = fgets($in);
     if (strlen($line)>3){
       $i++;
-      sscanf ($line, "%f %s", $A[$i], $legends[$i]);
-      $A[$i] = round ($A[$i], 0);
+      sscanf ($line, "%f %s", $A[ $i], $legends[ $i]);
+      $A[$i] = round ($A[ $i], 0);
     }
   }
 
   fclose($in);
   generatePie($Title, $A, $legends, $x_max, $y_max, $x0, $y0, $legend_size,
               $dia, $x0_legend, $y_legend);
+
 ?>
