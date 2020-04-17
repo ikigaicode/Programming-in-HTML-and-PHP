492d2993f2d1bdb42c1031605e7d316c33e17b4f
Lalisa <54802115+lalisaunadm@users.noreply.github.com>
2020-04-17 03:45:55 -0700

    comments

--- Unit 10/Ex 10a/hello.php
+++ Unit 10/Ex 10a/hello.php
@@ -2,4 +2,6 @@
   echo "Hello, world!";
 
   // C:\wamp64\bin\php\Ex 10a>C:\wamp64\bin\php\php7.2.4\php.exe hello.php
+  //command line to copy on my computer:
+  //                C:\wamp64\bin\php\php7.2.4\php.exe
 ?>
